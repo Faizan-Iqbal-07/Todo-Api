cffb19bd04d8e629c78a816af5c34c6cb845bf39
Faizan Iqbal <faizaniqbal2911@gmail.com>
2026-01-23 02:49:57 +0500

    first commit

---
+++ todo-backend/app.js
@@ -0,0 +1,32 @@
+
+const path = require('path');
+
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const cors = require('cors');
+const DB_PATH = "mongodb+srv://faizaniqbal2911_db_user:faizan313@clustertest.dqwpbhq.mongodb.net/todo?appName=ClusterTest";
+
+
+const todoItemsRouter = require("./routes/todoItemsRouter")
+const errorsController = require("./controllers/errors");
+
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/todo", todoItemsRouter);
+
+app.use(errorsController.pageNotFound);
+
+const PORT = 3001;
+
+mongoose.connect(DB_PATH).then(() => {
+  console.log('Connected to Mongo');
+  app.listen(PORT, () => {
+    console.log(`Server running on address http://localhost:${PORT}`);
+  });
+}).catch(err => {
+  console.log('Error while connecting to Mongo: ', err);
+});
